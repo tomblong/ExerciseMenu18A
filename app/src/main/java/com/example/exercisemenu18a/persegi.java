f8c2988721ae65804f85955306d4c03b01acfc26
tomblong <haniffauzan200015@gmail.com>
2020-04-06 21:59:59 +0700

    add masing masing activity

---
+++ app/src/main/java/com/example/exercisemenu18a/persegi.java
@@ -0,0 +1,14 @@
+package com.example.exercisemenu18a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class persegi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_persegi);
+    }
+}
---
+++ app/src/main/java/com/example/exercisemenu18a/segitiga.java
@@ -0,0 +1,14 @@
+package com.example.exercisemenu18a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class segitiga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segitiga);
+    }
+}
