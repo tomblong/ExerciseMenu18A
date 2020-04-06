e819087053547fe07a3727664f18678cab9133a0
tomblong <haniffauzan200015@gmail.com>
2020-04-06 21:44:23 +0700

    add bangun datar

--- app/src/main/java/com/example/exercisemenu18a/MainActivity.java
+++ app/src/main/java/com/example/exercisemenu18a/MainActivity.java
@@ -1,29 +1,47 @@
 package com.example.exercisemenu18a;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    EditText username, password;
+    Button btnok;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.btnok);
-        button.setOnClickListener(new View.OnClickListener() {
+        username = (EditText) findViewById(R.id.editUN);
+        password = (EditText) findViewById(R.id.editPass);
+        btnok = (Button) findViewById(R.id.btnok);
+
+        btnok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
+
+                String usernameKey = username.getText().toString();
+                String passwordKey = password.getText().toString();
+
+                if (usernameKey.equals("admin") && passwordKey.equals("123")){
+                    //jika login sukses
+                    Toast.makeText(getApplicationContext(), "LOGIN SUKSES LURRRRRR",
+                            Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Activity2.class);
+                    MainActivity.this.startActivity(intent);
+                }else {
+                    //jika login gagal
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Username atau Password anda salah lurrr !")
+                            .setNegativeButton("Retry",null).create().show();
+                }
             }
         });
-    }
-
-    public void openActivity2(){
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
     }
 }
